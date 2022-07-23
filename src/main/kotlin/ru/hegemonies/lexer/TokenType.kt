e43b7e4a7a166aa321777ccd07f3dc6eec07b91f
daniil dyachenko <hegemonies@yandex.ru>
2022-07-23 16:25:32 +0700

    Start adds token types for lexer

--- src/main/kotlin/ru/hegemonies/lexer/TokenType.kt
+++ src/main/kotlin/ru/hegemonies/lexer/TokenType.kt
@@ -1,5 +1,19 @@
 package ru.hegemonies.lexer
 
-enum class TokenType {
-    UNKNOWN,
+enum class TokenType(val token: String, val grammarName: String) {
+    START("", "start"),
+
+    L_CURVE_BRACE("{", ""),
+    L_DOUBLE_CURVE_BRACE("{{", "variable_open_symbol"),
+    R_CURVE_BRACE("}", ""),
+    R_DOUBLE_CURVE_BRACE("}}", "variable_close_symbol"),
+
+    EQUALS("=", ""),
+
+    DOUBLE_EQUALS("==", "conditional_expression_operator_operations"),
+    NOT_EQUALS("!=", "conditional_expression_operator_operations"),
+    STRONG_GREATER(">", "conditional_expression_operator_operations"),
+    GREATER_EQUALS(">=", "conditional_expression_operator_operations"),
+    STRONG_LESS("<", "conditional_expression_operator_operations"),
+    LESS_EQUALS("<=", "conditional_expression_operator_operations"),
 }
